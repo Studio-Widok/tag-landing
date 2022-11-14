15b9afcce0c0ad2a45926d5238fdf74ec6d7fc51
weterynarzfred <weterynarzfred@gmail.com>
2022-11-14 17:19:42 +0100

    add lazysizes and update

--- header.php
+++ header.php
@@ -17,15 +17,15 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Tytuł</title>
-  <link rel="icon" type="image/png" href="./media/favicon.png" />
+  <title>tytuł</title>
+  <!-- <link rel="icon" type="image/png" href="./media/favicon.png" /> -->
   <meta property="og:url" content="<?=CURRENT_URL?>" />
   <meta property="og:type" content="website" />
-  <meta property="og:title" content="tytuł" />
-  <meta property="og:description" content="opis" />
-  <meta property="og:image" content="<?=BASE_URL?>/media/ogimg.png" />
-  <meta property="og:image:width" content="512" />
-  <meta property="og:image:height" content="512" />
+  <!-- <meta property="og:title" content="tytuł" /> -->
+  <!-- <meta property="og:description" content="opis" /> -->
+  <!-- <meta property="og:image" content="<?=BASE_URL?>/media/ogimg.png" /> -->
+  <!-- <meta property="og:image:width" content="1200" />
+  <meta property="og:image:height" content="630" /> -->
   <meta name="color-scheme" content="light only">
   <link rel="stylesheet" href="./dist/main.css" />
 </head>
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,12 @@
-import widok from 'widok';
+
+// import 'lazysizes';
+// import smoothscroll from 'smoothscroll-polyfill';
+
+import 'widok';
+
+// smoothscroll.polyfill();
+
+// document.addEventListener('lazybeforeunveil', event => {
+//   const bg = event.target.getAttribute('data-bg');
+//   if (bg) event.target.style.backgroundImage = 'url(' + bg + ')';
+// });
